88d5e8c7831c9546a3eeb74adb8adbbef30c67ab
drpablovidal <drpablovidal@gmail.com>
2026-06-12 06:55:45 -0600

    Renombrar procedimientos inguinales

--- ingreso-cirugia/app.js
+++ ingreso-cirugia/app.js
@@ -38,9 +38,9 @@ const PROCEDURES = [
   "Colecistectomía por laparoscopia",
   "Colecistectomía por laparoscopia más colangiografía transoperatoria",
   "Colecistectomía por laparoscopia más colangiografía transoperatoria más identificación de tejidos por fluorescencia",
-  "Plastia inguinal bilateral con colocación de malla",
-  "Plastia inguinal por laparoscopia derecha con colocación de malla",
-  "Plastia inguinal por laparoscopia izquierda con colocación de malla",
+  "Plastia inguinal bilateral por laparoscopia más colocación de mallas",
+  "Plastia inguinal derecha por laparoscopia con colocación de malla",
+  "Plastia inguinal izquierda por laparoscopia más colocación de malla",
   "Plastia ventral por laparoscopia más colocación de malla (IPOM Plus)",
   "Funduplicatura tipo Toupet por laparoscopia",
   "Funduplicatura tipo Nissen por laparoscopia",
@@ -89,17 +89,17 @@ const DIAGNOSES_BY_PROCEDURE = {
     "Anatomía biliar compleja",
     "Síndrome de Mirizzi probable"
   ],
-  "Plastia inguinal bilateral con colocación de malla": [
+  "Plastia inguinal bilateral por laparoscopia más colocación de mallas": [
     "Hernia inguinal bilateral",
     "Hernia inguinal bilateral recidivante",
     "Hernia inguinoescrotal bilateral"
   ],
-  "Plastia inguinal por laparoscopia derecha con colocación de malla": [
+  "Plastia inguinal derecha por laparoscopia con colocación de malla": [
     "Hernia inguinal derecha",
     "Hernia inguinal derecha recidivante",
     "Hernia inguinoescrotal derecha"
   ],
-  "Plastia inguinal por laparoscopia izquierda con colocación de malla": [
+  "Plastia inguinal izquierda por laparoscopia más colocación de malla": [
     "Hernia inguinal izquierda",
     "Hernia inguinal izquierda recidivante",
     "Hernia inguinoescrotal izquierda"
